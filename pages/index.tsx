bb777df6430d3e8602c2b40443ede01b2a5a2438
Matt Sedmak <matt@digitalfans.se>
2022-05-30 17:48:26 +0200

    Added blocks to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,15 @@ import React from "react"
 import invariant from "tiny-invariant"
 
 import { parse } from "@/lib/utils/BlockParser"
-import { DescWithImgBlock } from "@components/descWithImgBlock"
+import { getImages } from "@/lib/utils/ImageGetter"
+import { Block } from "@components/blocks"
 import Layout from "@components/Layout/Layout"
-import { StatsBlock } from "@components/statsBlock"
+import { BlocksUnion } from "@models/blocks"
 import { GQLGlobalFields } from "@models/common"
 
 interface IndexProps {
   globalFields: GQLGlobalFields
+  blocks: BlocksUnion[]
 }
 
 export const getServerSideProps: GetServerSideProps<IndexProps> = async () => {
@@ -22,20 +24,19 @@ export const getServerSideProps: GetServerSideProps<IndexProps> = async () => {
 
   // ensure necessary data exist
   invariant(globalFields, "global fields is undefined")
+  invariant(pageBlocks, "Page blocks are undefined")
 
-  parse(pageBlocks.page.blocks)
+  const { blocks, imagesIds } = parse(pageBlocks.page.blocks)
+  const images = await getImages(imagesIds)
 
-  return { props: { globalFields } }
+  return { props: { globalFields, blocks } }
 }
 
-export default function Index({ globalFields }: IndexProps) {
+export default function Index({ globalFields, blocks }: IndexProps) {
   const { acfGlobalFields, generalSettings } = globalFields
   return (
     <Layout acfGlobalFields={acfGlobalFields} generalSettings={generalSettings}>
-      <div>
-        <StatsBlock />
-        <DescWithImgBlock />
-      </div>
+      {blocks ? blocks.map(block => <Block key={block.name} block={block} />) : null}
     </Layout>
   )
 }
