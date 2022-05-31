c5b7cd41ec808922c75291da51847ae9bf775d5e
Matt Sedmak <matt@digitalfans.se>
2022-05-31 10:27:35 +0200

    connected context for images + pageids

--- lib/components/blocks/StatsBlock.tsx
+++ lib/components/blocks/StatsBlock.tsx
@@ -1,36 +1,20 @@
+import { useGlobalContext } from "@context/global"
 import Image from "next/image"
 
-import image1 from "@/public/images/Centralized _Network.svg"
-import image2 from "@/public/images/Detective.svg"
-import image3 from "@/public/images/Earth_Care.svg"
 import { AppLink } from "@components/shared/AppLink"
 import { Wysiwyg } from "@components/shared/Wysiwyg"
 import { ArrowRight } from "@icons/ArrowRight"
 import { StatsData } from "@models/blocks"
 
-const MOCKDATA = [
-  {
-    imageSrc: image1,
-    title: "Well developed networks",
-  },
-  {
-    imageSrc: image2,
-    title: "Deep market understanding",
-  },
-  {
-    imageSrc: image3,
-    title: "Global presence",
-  },
-]
+export const StatsBlock = ({ heading, description, linkTitle, linkUri, gallery }: StatsData) => {
+  const { images, pageMap } = useGlobalContext()
 
-export const StatsBlock = ({ heading, description, linkTitle, gallery }: StatsData) => {
-  // TODO: Connect backend and remove mock data.
-  const _stats = MOCKDATA.map(stat => (
-    <div key={stat.title} className="flex w-[427px] flex-col items-center">
+  const _stats = gallery.map(({ imageId, title }) => (
+    <div key={title} className="flex w-[427px] flex-col items-center">
       <div className="relative mb-[28px] aspect-square h-[96px] w-[96px]">
-        <Image src={stat.imageSrc} alt="" layout="fill" objectFit="cover" />
+        <Image {...images![imageId]} alt="" layout="fill" objectFit="cover" />
       </div>
-      <h4 className="app-h4 text-dark-green">{stat.title}</h4>
+      <h4 className="app-h4 text-dark-green">{title}</h4>
     </div>
   ))
 
@@ -42,7 +26,9 @@ export const StatsBlock = ({ heading, description, linkTitle, gallery }: StatsDa
 
       <div className="mb-20 flex w-full justify-between space-x-8">{_stats}</div>
       <div className="flex items-center space-x-[10px]">
-        <AppLink className="link-m">{linkTitle}</AppLink>
+        <AppLink href={pageMap![linkUri]!.uri} className="link-m">
+          {linkTitle}
+        </AppLink>
         <ArrowRight />
       </div>
     </section>
