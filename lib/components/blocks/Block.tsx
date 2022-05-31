53343bd003563395ec058489afd5ea9b92bf97d5
Matt Sedmak <matt@digitalfans.se>
2022-05-31 13:18:20 +0200

    added the logowall block

--- lib/components/blocks/Block.tsx
+++ lib/components/blocks/Block.tsx
@@ -1,13 +1,17 @@
 import Hero from "@components/hero/Hero"
-import { BaseBlock, isDescWithImageData, isHeroData, isStatsData } from "@models/blocks"
+import {
+    BaseBlock, isDescWithImageData, isHeroData, isLogowallData, isStatsData
+} from "@models/blocks"
 
 import { DescWithImgBlock } from "./"
+import { LogowallBlock } from "./LogowallBlock"
 import { StatsBlock } from "./StatsBlock"
 
 export const Block = ({ block }: { block: BaseBlock }) => {
   if (isHeroData(block)) return <Hero {...block} />
   if (isStatsData(block)) return <StatsBlock {...block} />
   if (isDescWithImageData(block)) return <DescWithImgBlock {...block} />
+  if (isLogowallData(block)) return <LogowallBlock {...block} />
 
   return null
 }
