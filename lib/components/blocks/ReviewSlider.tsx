9333a95d4269349cb8ce5b2c88749fc20dfc0bf7
Matt Sedmak <matt@digitalfans.se>
2022-05-31 17:30:55 +0200

    Updated block with Swiper

--- lib/components/blocks/ReviewSlider.tsx
+++ lib/components/blocks/ReviewSlider.tsx
@@ -1,3 +1,29 @@
-export const ReviewSlider = () => {
-  return <div>ReviewSlider</div>
+import { Pagination } from "swiper"
+import { Swiper, SwiperSlide } from "swiper/react"
+
+import { ReviewSliderData } from "@models/blocks"
+
+export const ReviewSlider = ({ reviews }: ReviewSliderData) => {
+  const _slides = reviews.map(review => (
+    <SwiperSlide key={review.reviewClient}>
+      <div className="mx-auto flex max-w-[884px] flex-col items-center">
+        <span className="quote-icon">"</span>
+        <p className="quote-l mb-8 text-center">{review.reviewText}</p>
+        <div className="mb-8">
+          <span className="text-body-l font-normal">{review.reviewClient}, </span>
+          <span className="text-body-l">{review.reviewCompany}</span>
+        </div>
+      </div>
+    </SwiperSlide>
+  ))
+
+  return (
+    <Swiper
+      pagination={{ clickable: true, type: "bullets" }}
+      modules={[Pagination]}
+      className="bg-light-beige pt-[60px]"
+    >
+      {_slides}
+    </Swiper>
+  )
 }
