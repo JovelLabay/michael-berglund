fdbb18dc7d9c89da4fdad10ed0fa4ab675824bca
Matt Sedmak <matt@digitalfans.se>
2022-06-01 15:12:44 +0200

    Created new desc block

--- lib/components/blocks/Block.tsx
+++ lib/components/blocks/Block.tsx
@@ -1,7 +1,6 @@
-
 import {
     BaseBlock, isDescWithImageData, isHeroData, isLogowallData, isRelatedArticlesData,
-    isReviewSilderData, isStatsData
+    isReviewSilderData, isShortDescData, isStatsData
 } from "@models/blocks"
 
 import { DescWithImgBlock } from "./"
@@ -9,18 +8,17 @@ import Hero from "./hero/Hero"
 import { LogowallBlock } from "./LogowallBlock"
 import { RelatedArticles } from "./RelatedArticles"
 import { ReviewSlider } from "./ReviewSlider"
+import { ShortDescBlock } from "./ShortDescBlock"
 import { StatsBlock } from "./StatsBlock"
 
 export const Block = ({ block }: { block: BaseBlock }) => {
   if (isHeroData(block)) return <Hero {...block} />
   if (isStatsData(block)) return <StatsBlock {...block} />
   if (isDescWithImageData(block)) return <DescWithImgBlock {...block} />
-
   if (isLogowallData(block)) return <LogowallBlock {...block} />
   if (isReviewSilderData(block)) return <ReviewSlider {...block} />
-
   if (isRelatedArticlesData(block)) return <RelatedArticles {...block} />
-
+  if (isShortDescData(block)) return <ShortDescBlock {...block} />
 
   return null
 }
