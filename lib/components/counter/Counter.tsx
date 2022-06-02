a4e08c2767085f84c6145cefa52c8c4800f8dfe0
Matt Sedmak <matt@digitalfans.se>
2022-06-02 14:53:42 +0200

    Created counter component.

---
+++ lib/components/counter/Counter.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react"
+
+interface CounterProps {
+  pointNumber: number
+  pointSymbol: string
+  pointTitle: string
+}
+
+//Easing function.
+const easeOutQuad = (t: number) => t * (2 - t)
+
+export const Counter = ({ pointNumber, pointSymbol, pointTitle }: CounterProps) => {
+  const [count, setCount] = useState(0)
+  const duration = 2000
+  const frameDuration = 1000 / 60
+
+  useEffect(() => {
+    let frame = 0
+
+    const totalFrames = Math.round(duration / frameDuration)
+
+    const timer = setInterval(() => {
+      frame++
+      const progress = easeOutQuad(frame / totalFrames)
+      setCount(pointNumber * progress)
+
+      if (frame === totalFrames) {
+        clearInterval(timer)
+      }
+    }, frameDuration)
+
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
+
+  return (
+    <div className="flex w-[309px] flex-col items-center">
+      <div className="app-h1 mb-4 text-dark-beige">
+        <span>{Math.floor(count)}</span>
+        {pointSymbol && <span>{pointSymbol}</span>}
+      </div>
+      <div className="body-l text-dark-beige">{pointTitle}</div>
+    </div>
+  )
+}
