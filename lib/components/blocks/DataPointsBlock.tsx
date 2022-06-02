0bb42b68316345da328a354b6befadbdce3cb5f4
Matt Sedmak <matt@digitalfans.se>
2022-06-02 14:54:03 +0200

    Added coounter + animations to block.

--- lib/components/blocks/DataPointsBlock.tsx
+++ lib/components/blocks/DataPointsBlock.tsx
@@ -1,19 +1,45 @@
+import { useEffect, useMemo, useRef, useState } from "react"
+
+import { Counter } from "@components/counter"
 import { DataPointsData } from "@models/blocks"
 
 export const DataPointsBlock = ({ points }: DataPointsData) => {
-  const _points = points.map(point => (
-    <div className="flex w-[309px] flex-col items-center">
-      <div className="app-h1 mb-4 text-dark-beige">
-        <span>{point.pointNumber}</span>
-        {point.pointSymbol && <span>{point.pointSymbol}</span>}
-      </div>
-      <div className="body-l text-dark-beige">{point.pointTitle}</div>
-    </div>
-  ))
+  const containerRef = useRef<HTMLDivElement>(null)
+  const [isVisible, setIsVisible] = useState(false)
+
+  const startCountAnimation = (entries: IntersectionObserverEntry[]) => {
+    const [entry] = entries
+    setIsVisible(entry.isIntersecting)
+  }
+
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.2,
+  }
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(startCountAnimation, options)
+
+    if (containerRef.current) observer.observe(containerRef.current)
+
+    return () => {
+      if (containerRef.current) observer.unobserve(containerRef.current)
+    }
+  }, [containerRef, options])
+
+  // Build the points.
+  const _points = useMemo(
+    () => points.map(point => <Counter key={point.pointNumber} {...point} />),
+    [points]
+  )
 
   return (
-    <div className="flex items-center justify-between bg-light-beige px-12 pt-[100px] pb-[120px]">
-      {_points}
+    <div
+      ref={containerRef}
+      className="flex items-center justify-between bg-light-beige px-12 pt-[100px] pb-[120px]"
+    >
+      {isVisible && _points}
     </div>
   )
 }
