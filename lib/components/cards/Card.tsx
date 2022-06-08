17a43c7a63b8aa1e69778e3a5e8cd1db9f51d4ca
Clifford <cliffordl@corebridge.net>
2022-06-08 15:55:16 +0800

    MICH0001-51 | Remove class name

--- lib/components/cards/Card.tsx
+++ lib/components/cards/Card.tsx
@@ -14,7 +14,7 @@ export interface CardProps {
 
 export const Card = ({ image, body, link, className }: CardProps) => {
   const card = (
-    <div className={classNames("relative h-full",{ 'card-wrapper': !link })}>
+    <div className={classNames("relative h-full")}>
       {/* Overlay */}
       <div className="absolute top-0 z-0 h-full w-full bg-darker-beige"></div>
 
@@ -34,5 +34,5 @@ export const Card = ({ image, body, link, className }: CardProps) => {
     </div>
   )
 
-  return link ? <AppLink className='card-wrapper' href={link}>{card}</AppLink> : card
+  return link ? <AppLink href={link}>{card}</AppLink> : card
 }
