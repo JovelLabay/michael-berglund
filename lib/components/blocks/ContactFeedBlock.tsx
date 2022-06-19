32337fb46e10608584073da981fcf56836f3ff79
Pete <pete@digitalfans.se>
2022-06-19 14:33:43 +0200

    followed card design from figma

--- lib/components/blocks/ContactFeedBlock.tsx
+++ lib/components/blocks/ContactFeedBlock.tsx
@@ -6,14 +6,20 @@ import { ContactFeedListblock } from "@models/blocks"
 import { MedarbetarePost } from "@models/common"
 
 export const ContactFeedBlock = ({ contactLists, coverPhotoId }: ContactFeedListblock) => {
-  const { images, postMap } = useGlobalContext();
-  const coverPhoto = images && coverPhotoId ? images[coverPhotoId] : undefined;
-  
-  return <>{
-      contactLists && contactLists.map(data => {
-      return (<section className="section-padding bg-white w-full " key={data.title}> 
+  const { images, postMap } = useGlobalContext()
+  const coverPhoto = images && coverPhotoId ? images[coverPhotoId] : undefined
+
+  console.log(coverPhoto)
+
+  return (
+    <>
+      {contactLists &&
+        contactLists.map(data => {
+          return (
+            <section className="section-padding w-full bg-white " key={data.title}>
+              <div className="mx-auto max-w-[1440px] xl:px-12">
                 <h3 className="app-h3 text-left ">{data.title}</h3>
-                <div className="grid grid-cols-1 pt-[60px] gap-8 md:grid-cols-3 md:gap-4  lg:grid-cols-3 lg:gap-8 ">
+                <div className="grid grid-cols-1 gap-8 pt-[60px] md:grid-cols-2 md:gap-4  lg:grid-cols-3 lg:gap-8 ">
                   {data.medarbetareIds &&
                     data.medarbetareIds.map(postId => {
                       const {
@@ -23,24 +29,26 @@ export const ContactFeedBlock = ({ contactLists, coverPhotoId }: ContactFeedList
                         featuredImage: { node: image },
                       } = postMap![postId] as MedarbetarePost
                       return (
-                          <ProfileCard
-                            key={id}
-                            image={image}
-                            name={title}
-                            position={position}
-                            email={email}
-                            phone={phone}
-                            linkedin={linkedin}
-                            bio={bio}
-                            coverPhoto={coverPhoto}
-                          />
+                        <ProfileCard
+                          key={id}
+                          image={image}
+                          name={title}
+                          position={position}
+                          email={email}
+                          phone={phone}
+                          linkedin={linkedin}
+                          bio={bio}
+                          coverPhoto={coverPhoto}
+                        />
                       )
                     })}
                 </div>
-                
-                <div className="pt-[100px] border-b w-full"></div>
-          </section>)
-        })
-   
-      }</>
+
+                <div className="w-full border-b pt-[100px]"></div>
+              </div>
+            </section>
+          )
+        })}
+    </>
+  )
 }
--- lib/components/cards/ProfileCard.tsx
+++ lib/components/cards/ProfileCard.tsx
@@ -2,10 +2,11 @@ import classNames from "classnames"
 import Image from "next/image"
 import React, { useState } from "react"
 
+import { AppLink } from "@components/shared/AppLink"
 import { Wysiwyg } from "@components/shared/Wysiwyg"
 import { FlipIcon } from "@icons/FlipIcon"
 import { LinkedinIcon } from "@icons/LinkedinIcon"
-import { ImageMap, WPMedia } from "@models/common"
+import { WPMedia } from "@models/common"
 
 export interface ProfileItemProps {
   image: WPMedia
@@ -34,89 +35,98 @@ export const ProfileCard = ({
   }
 
   return (
-    <div className="relative h-[650px] ">
-      <div className="profile-card absolute float-left w-full">
+    <div className="relative min-h-[520px] xsm:min-h-[700px] md:min-h-[540px]">
+      <div className="profile-card absolute h-full w-full">
         <div
           className={classNames(
-            isActive ? "transform" : "",
-            "content absolute h-full w-full transition-transform duration-1000 ease-in-out"
+            "content absolute h-full w-full transition-transform duration-1000 ease-in-out",
+            {
+              transform: isActive,
+            }
           )}
         >
           <div
             className={classNames(
               isActive ? "z-0" : "",
-              "front absolute h-[650px] w-full bg-light-beige"
+              "front absolute min-h-[520px] w-full xsm:min-h-[700px] md:min-h-[540px] "
             )}
           >
-            <div className="relative mb-5 aspect-square  w-full shrink-0  ">
-              <div className="absolute top-0 z-0 h-full w-full "></div>
-              <div className=" w-full translate-x-0.5 -translate-y-0.5 transition ease-in-out hover:translate-x-1 hover:-translate-y-1">
-                {image && (
-                  <div className="relative aspect-video h-[390px] w-full">
-                    <Image
-                      src={image.mediaItemUrl}
-                      alt={image.altText}
-                      layout="fill"
-                      objectFit="cover"
-                    />
-                  </div>
-                )}
-                <div className="dark-blue min-h-[228px] p-8 pt-[36px]">
-                  <h5 className="app-h4 ">{name}</h5>
-                  <span className="pre-title  block w-full  pt-[16px] font-medium uppercase tracking-[0.15em]">
-                    {position}
-                  </span>
-                  <span className="pre-title block  w-full break-all pt-[20px] font-gotham font-[350] tracking-[0.15em]">
-                    {email}
-                  </span>
-                  <span className="pre-title block w-full pt-[8px] font-gotham font-[350] tracking-[0.15em]">
-                    {phone}
-                  </span>
-                  <span className="flex w-full pt-[22px]">
-                    <span
-                      className="link-m text-base inline-flex w-40  cursor-pointer font-medium"
-                      onClick={handleClick}
-                    >
-                      Read Bio &nbsp; <FlipIcon className="relative" />
-                    </span>
-                    <span className="pre-title  text-base w-full  text-right font-medium tracking-[0.15em] ">
-                      <a href={linkedin} target="_blank" rel="noreferrer">
-                        <LinkedinIcon className="float-right" />
-                      </a>
-                    </span>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="back absolute inline-block h-[650px] w-full bg-light-beige">
-            <div className=" mb-5 aspect-square  h-full w-full shrink-0 bg-light-beige">
-              <div
-                style={{ backgroundImage: `url(${coverPhoto.src})` }}
-                className="absolute -top-px h-14 w-full"
-              ></div>
-              <br />
-              <div className="relative m-auto aspect-square  w-[64px] shrink-0 overflow-hidden  rounded-full border-2 border-white">
-                {image && (
+            <div className="absolute top-0 z-0 h-full w-full bg-darker-beige"></div>
+            <div className="absolute flex h-full w-full translate-x-0.5 -translate-y-0.5 flex-col bg-light-beige transition ease-in-out hover:translate-x-1 hover:-translate-y-1">
+              {image && (
+                <div className="aspect-h-[220] aspect-w-[335] relative max-h-[300px] w-full lg:aspect-h-[300] lg:aspect-w-[427]">
                   <Image
                     src={image.mediaItemUrl}
                     alt={image.altText}
                     layout="fill"
                     objectFit="cover"
                   />
-                )}
+                </div>
+              )}
+              <div className="dark-blue flex flex-1 flex-col p-5 pt-6 lg:p-8">
+                <div className="flex-1">
+                  <h5 className="app-h4 ">{name}</h5>
+                  <span className="pre-title  block w-full  pt-[16px] font-[350] uppercase tracking-[0.15em]">
+                    {position}
+                  </span>
+
+                  <a href={`mailto:${email}`}>
+                    <span className="block  w-full break-all pt-[20px] tracking-[0.15em]">
+                      {email}
+                    </span>
+                  </a>
+                  <a href={`tel:${phone}`}>
+                    <span className="block w-full pt-1 tracking-[0.15em]">{phone}</span>
+                  </a>
+                </div>
+
+                <span className="flex w-full justify-between pt-[22px]">
+                  <span
+                    className="link-m text-base inline-flex w-40  cursor-pointer font-medium"
+                    onClick={handleClick}
+                  >
+                    Read Bio &nbsp; <FlipIcon className="relative" />
+                  </span>
+
+                  {linkedin && (
+                    <AppLink href={linkedin}>
+                      <LinkedinIcon />
+                    </AppLink>
+                  )}
+                </span>
               </div>
-              <span className="dark-blue block w-full break-all p-[20px] text-center font-medium uppercase tracking-[0.15em]">
-                <h5 className="app-h4 pb-5 text-center ">{name}</h5>
+            </div>
+          </div>
+          <div className="back absolute flex min-h-[520px] w-full flex-col bg-light-beige xsm:min-h-[700px] md:min-h-[540px]">
+            <div className="absolute top-0 z-0 h-full w-full bg-darker-beige"></div>
+            <div className="absolute flex h-full w-full translate-x-0.5 -translate-y-0.5 flex-col bg-light-beige transition ease-in-out hover:translate-x-1 hover:-translate-y-1">
+              <div className="relative h-16 shrink-0">
+                {/* TODO: Needs to be updated! */}
+                <div
+                  style={{ backgroundImage: `url(${coverPhoto.src})` }}
+                  className="absolute h-full w-full"
+                ></div>
+                <div className="absolute top-7 left-1/2 aspect-square w-[64px]  -translate-x-1/2 overflow-hidden  rounded-full border border-white">
+                  {image && (
+                    <Image
+                      src={image.mediaItemUrl}
+                      alt={image.altText}
+                      layout="fill"
+                      objectFit="cover"
+                    />
+                  )}
+                </div>
+              </div>
+              <h5 className="app-h4 mt-[52px] text-center">{name}</h5>
+              <span className="pre-title mt-4 block text-center font-[350] uppercase tracking-[0.15em]">
                 {position}
               </span>
               <Wysiwyg
                 content={bio}
-                hasWysiwyg={false}
-                className="dark-blue min-h-[330px]  overflow-auto p-8 pt-[32px]"
+                className="prose-p:body-m prose flex-1 p-5 py-5 lg:px-[36px]"
               />
               <span
-                className="link-m text-base  relative inline-flex w-48 cursor-pointer p-8 pt-[38px] font-medium"
+                className="link-m text-base  relative inline-flex w-48 cursor-pointer p-5 pt-0 font-medium lg:p-8"
                 onClick={handleClick}
               >
                 Contact Info &nbsp; <FlipIcon className="relative" />
