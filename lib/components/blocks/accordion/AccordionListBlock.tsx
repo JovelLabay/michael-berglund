19ae179ce8fb596a2652634a9ff3298b25b7e1c1
Matt Sedmak <matt@digitalfans.se>
2022-07-11 10:53:17 +0200

    Adjusted spacings for accordian

--- lib/components/blocks/accordion/AccordionListBlock.tsx
+++ lib/components/blocks/accordion/AccordionListBlock.tsx
@@ -1,12 +1,17 @@
+import classNames from "classnames"
+
 import { AccordionListsData } from "@models/blocks"
 
 import { AccordionItem } from "./AccordionItem"
 
 export const AccordionListBlock = ({ accordionLists }: AccordionListsData) => {
   return (
-    <section className="w-full bg-white px-5 pb-[40px] lg:px-12">
+    <section className="w-full bg-white px-5 pb-0 lg:px-12 lg:pb-10">
       {accordionLists.map((accordion, index) => (
-        <div key={accordion.groupTitle + index} className="w-full pt-[60px]">
+        <div
+          key={accordion.groupTitle + index}
+          className={classNames("w-full pt-[60px] lg:pt-[100px]", { "lg:pt-20": index !== 0 })}
+        >
           <h3 className="app-h3 mb-10 lg:mb-[60px]">{accordion.groupTitle}</h3>
           <AccordionItem accordionGroupDataList={accordion.accordionLists}></AccordionItem>
 
