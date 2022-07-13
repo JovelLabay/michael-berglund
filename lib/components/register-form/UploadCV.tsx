64b639c771d513893a72ef9dc9480fc2e90030d6
Matt Sedmak <matt@digitalfans.se>
2022-07-13 17:14:34 +0200

    Fixes to uploadCv

--- lib/components/register-form/UploadCV.tsx
+++ lib/components/register-form/UploadCV.tsx
@@ -1,37 +1,70 @@
 import classNames from "classnames"
-import { SyntheticEvent } from "react"
+import { SyntheticEvent, useEffect, useState } from "react"
+import { UseFormRegisterReturn } from "react-hook-form"
 
 import { AddFileIcon } from "@icons/AddFileIcon"
+import { CloseIcon } from "@icons/CloseIcon"
 
 interface UploadCVProps {
-  upload: (e: SyntheticEvent) => void
   classname?: string
+  formRegister: UseFormRegisterReturn
+  forId: string
 }
 
-export const UploadCV = ({ upload, classname }: UploadCVProps) => {
+export const UploadCV = ({ classname, formRegister, forId }: UploadCVProps) => {
+  const [cvFileName, setCvFileName] = useState("")
+  const [isFiles, setIsFiles] = useState(false)
+
+  const { onChange, ...params } = formRegister
+
+  const handleCvUpload = (e: SyntheticEvent) => {
+    const target = e.target as HTMLInputElement
+
+    if (target.files && target.files.length > 0) {
+      setCvFileName(target.files[0].name)
+      setIsFiles(true)
+    }
+    onChange(e)
+  }
+
+  const removeFile = (e: SyntheticEvent) => {
+    e.stopPropagation()
+    e.preventDefault()
+    setIsFiles(false)
+  }
+
   return (
-    <div className={classNames("mb-10 flex flex-col", classname)}>
-      <h4 className="pre-title mb-4 uppercase text-light-green"> Ladda upp ditt CV</h4>
-      <label
-        htmlFor="uploadCv"
-        className="form-input flex cursor-pointer flex-col items-center justify-center text-white outline outline-white xl:flex-row"
-      >
-        <div className="mb-1 flex lg:mb-0">
-          <AddFileIcon className="mr-[10px]" />
-          <span className="link-m pr-2">Ladda upp eller lägg till fil</span>{" "}
-        </div>
-        <span className="link-s text-medium-green"> pdf, doc, doxc, jpg · max 10 mb</span>
-        <input
-          id="uploadCv"
-          type="file"
-          multiple={false}
-          accept=".doc,.docx,.pdf,.jpeg,.jpg,.png"
-          onChange={upload}
-          className="hidden"
-        />
-      </label>
-      {/* TODO: show file name. */}
-      {/* <span className="text-white">File name here</span> */}
-    </div>
+    <label
+      htmlFor={forId}
+      className={classNames(
+        "flex cursor-pointer flex-col items-center justify-center rounded-sm py-4 px-4 text-white outline outline-white xl:flex-row",
+        classname
+      )}
+    >
+      <div className="mb-1 flex lg:mb-0">
+        <AddFileIcon className="mr-[10px]" />
+        <span className="link-m pr-2">
+          {isFiles ? cvFileName : "Ladda upp eller lägg till fil"}
+        </span>
+      </div>
+      <span className="link-s text-medium-green">
+        {isFiles ? (
+          <button onClick={removeFile} className="flex items-center">
+            <CloseIcon />
+          </button>
+        ) : (
+          "pdf, doc, doxc, jpg · max 10 mb"
+        )}
+      </span>
+      <input
+        id={forId}
+        {...params}
+        type="file"
+        multiple={false}
+        accept=".doc,.docx,.pdf,.jpeg,.jpg,.png"
+        className="hidden"
+        onChange={handleCvUpload}
+      />
+    </label>
   )
 }
