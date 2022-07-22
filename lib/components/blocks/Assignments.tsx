5bcd56c72a5748033ed06346245bf2ad9f41dd15
jovellabay <jovelhatchitsolutions@gmail.com>
2022-07-22 16:20:05 +0800

    MICH0001-238 | Completed Assignments - Rework | Remove the functionality that remains the current list.

--- lib/components/blocks/Assignments.tsx
+++ lib/components/blocks/Assignments.tsx
@@ -16,9 +16,6 @@ import AssignmentCheck from "@icons/AssignmentCheck"
 export const Assignments = ({ title, assignments }: AssignmentsData) => {
   const responsiveLG = useResponsiveLG()
 
-  const slidesPerView = responsiveLG ? 2.02 : 1
-  const totalPages = Math.ceil(assignments.length / Math.floor(slidesPerView))
-
   const [assignmentList, setAssignmentList] = useState<AssignmentsData["assignments"]>([])
   const [limiter, setLimiter] = useState(3)
 
@@ -26,38 +23,8 @@ export const Assignments = ({ title, assignments }: AssignmentsData) => {
   const [modelContent, setModelContent] = useState<AssignmentsDataPopUp>()
 
   useEffect(() => {
-    const assignmentListItems = localStorage.getItem("assignments") || "3"
-
-    if (localStorage.getItem("assignments") === null) {
-      localStorage.setItem("assignments", JSON.stringify(3))
-    }
-
-    setAssignmentList(assignments.slice(0, JSON.parse(assignmentListItems)))
+    setAssignmentList(assignments.slice(0, limiter))
   }, [limiter])
-
-  // const slides = useMemo(
-  //   () =>
-  //     assignments.map(({ title, description }, index) => (
-  //       <SwiperSlide key={index}>
-  //         <div className="item flex h-full flex-col lg:flex-row">
-  //           <div className="relative h-[64px] w-[100%] shrink-0 lg:aspect-square lg:h-full lg:w-[28.05%]">
-  //             <Image
-  //               src={AssignmentBg.src}
-  //               alt="Completed Assignments BG"
-  //               layout="fill"
-  //               objectFit="cover"
-  //             />
-  //             <CheckMark className="absolute left-12 top-8 h-5 w-[28px] -translate-x-1/2 -translate-y-1/2 fill-white lg:top-1/2 lg:left-1/2 lg:h-[32px] lg:w-[52px]" />
-  //           </div>
-  //           <div className="flex w-full flex-col justify-center bg-light-beige p-8">
-  //             <h5 className="app-h4 mb-5">{title}</h5>
-  //             <Wysiwyg content={description} />
-  //           </div>
-  //         </div>
-  //       </SwiperSlide>
-  //     )),
-  //   [assignments]
-  // )
 
   return (
     <section className="section-padding relative bg-white pb-[120px] lg:pl-12 lg:pr-0">
@@ -65,7 +32,6 @@ export const Assignments = ({ title, assignments }: AssignmentsData) => {
         <hr className="bg-darker-beige" />
       </div>
       <h3 className="app-h3 mb-10 lg:mb-[60px]">{title}</h3>
-      {/* <ProgressSwiper totalPages={totalPages} slidesPerView={slidesPerView} slides={slides} /> */}
 
       {/* ASSIGNMENT CONTAINERS */}
       <div className="mr-0 grid grid-cols-1 gap-7 md:mr-14 md:grid-cols-3">
@@ -96,7 +62,6 @@ export const Assignments = ({ title, assignments }: AssignmentsData) => {
       <button
         className="mx-auto mt-10 flex flex-row items-center"
         onClick={() => {
-          localStorage.setItem("assignments", JSON.stringify(limiter + 3))
           setLimiter(prev => prev + 3)
         }}
       >
