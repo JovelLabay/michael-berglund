54118561c0b99be8a1bfc5baabfad29d19232b3a
jovellabay <jovelhatchitsolutions@gmail.com>
2022-08-02 10:16:57 +0800

    MICH0001-300 | Course - category - acf - its better if category type is dropdown MICH0001-301 | Course feed - responsive - too tight on table screens MICH0001-302 |Course card - default layout of some cards looks like the hover state

--- lib/components/cards/CardLink.tsx
+++ lib/components/cards/CardLink.tsx
@@ -14,7 +14,7 @@ export const CardLink = ({ link, card }: CardLinkProps) => {
         <div className="relative h-full">
           {/* Overlay */}
           <div className="absolute top-0 z-0 h-full w-full bg-darker-beige"></div>
-          <div className="h-full w-full translate-x-0.5 -translate-y-0.5 transition ease-in-out hover:translate-x-1 hover:-translate-y-1">
+          <div className="h-full w-full translate-x-0.5 -translate-y-0.5 bg-light-beige shadow-shadow-cus-2 transition ease-in-out hover:translate-x-1 hover:-translate-y-1">
             {card}
           </div>
         </div>
