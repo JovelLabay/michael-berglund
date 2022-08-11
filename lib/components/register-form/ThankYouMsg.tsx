377c9a15b4bd8d0ca6bdbc974f985369b33b0d54
Matt Sedmak <matt@digitalfans.se>
2022-08-11 11:21:05 +0200

    MICH0001-272 - added min height

--- lib/components/register-form/ThankYouMsg.tsx
+++ lib/components/register-form/ThankYouMsg.tsx
@@ -1,6 +1,6 @@
 export const ThankYouMsg = () => {
   return (
-    <div className="">
+    <div className="min-h-[700px]">
       <h4 className="app-h4 mb-8 text-white">Thank you for registering</h4>
       <p className="body-m text-white">
         Short text confirming the registration has been completed and information about the next
